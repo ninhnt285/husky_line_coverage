16804b7274b50436a1d7a3a55a3a81d56ced11dd
Ninh Nguyen <tnguy248@uncc.edu>
2022-06-21 15:20:57 -0400

    Support rotate and scale

--- scripts/create_map.py
+++ scripts/create_map.py
@@ -5,50 +5,60 @@ import cv2
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 from std_msgs.msg import ColorRGBA
+from math import cos, pi, sin
 
 if __name__ == "__main__":
     rospy.init_node('rviz_marker')
     marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=10)
 
     # Load map
-    map = cv2.imread("./maps/uncc.png", cv2.COLOR_BGR2RGB)
+    map = cv2.imread("./maps/woodward.png", cv2.COLOR_BGR2RGB)
+
     map_size = map.shape
-    ratio = 100
-    scale = 1.0 / float(ratio)
+    scale = 0.2
     step = 5
     print(map_size)
 
-    diff_pos = Point(0.0, 0.0, 0.0)
+
+    center_point = Point(205.0, 510.0, 0.0)
+    rotate_angle = -49.0
+
 
     image = Marker()
-    image.header.frame_id = "my_frame"
+    image.header.frame_id = "map"
     image.id = 0
     image.type = Marker.POINTS
+    image.action = Marker.MODIFY
 
+    image.pose.orientation.z = pi / 180.0 * rotate_angle
     image.pose.orientation.w = 1.0
+
     image.pose.position.x = 0.0
     image.pose.position.y = 0.0
     image.pose.position.z = 0.0
 
-    image.scale.x = scale * step
-    image.scale.y = scale * step
+    image.scale.x = 1
+    image.scale.y = 1
     image.scale.z = 0.01
 
     image.ns = "map"
 
     for r in range(0, map_size[0], step):
         for c in range(0, map_size[1], step):
+            x = float(c) - center_point.x
+            y = center_point.y - float(r)
+
             color = ColorRGBA()
             color.r = float(map[r][c][0]) / 255.0
             color.g = float(map[r][c][1]) / 255.0
             color.b = float(map[r][c][2]) / 255.0
-            color.a = 0.2
+            color.a = 0.5
             image.colors.append(color)
 
             p = Point()
-            p.x = diff_pos.x - float(c) * scale
-            p.y = diff_pos.y - float(r) * scale
-            p.z = -0.05
+            p.x = x * scale
+            p.y = y * scale
+            p.z = 0
             image.points.append(p)
 
 
